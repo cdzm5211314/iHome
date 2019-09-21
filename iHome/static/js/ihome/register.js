61f060fa7aff52245ceb2e2fecf919841b8054c7
cdzm5211314 <123309778@qq.com>
2019-09-21 14:12:44 +0800

    图片验证码的前端编写

--- iHome/static/js/ihome/register.js
+++ iHome/static/js/ihome/register.js
@@ -3,6 +3,7 @@ function getCookie(name) {
     return r ? r[1] : undefined;
 }
 
+// 保存图片验证码编号的全局变量
 var imageCodeId = "";
 
 function generateUUID() {
@@ -19,6 +20,13 @@ function generateUUID() {
 }
 
 function generateImageCode() {
+    // 形成图片验证码的后端地址,设置到页面中,让浏览器请求验证码图片
+    // 1.生成图片验证码编号
+    imageCodeId = generateUUID();
+    // 2.拼接获取验证码图片的url
+    var url = "/api/v1.0/image_codes/" + imageCodeId
+    $(".image-code img").attr("src",url);
+
 }
 
 function sendSMSCode() {
