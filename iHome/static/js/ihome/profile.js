3cd016392d673af5b2a4457835716bbfffff58a7
cdzm5211314 <123309778@qq.com>
2019-09-23 20:58:45 +0800

    上传用户头像的前端编写

--- iHome/static/js/ihome/profile.js
+++ iHome/static/js/ihome/profile.js
@@ -11,3 +11,30 @@ function getCookie(name) {
     return r ? r[1] : undefined;
 }
 
+// 页面加载完成后执行
+$(document).ready(function () {
+    $("#form-avatar").submit(function (e) {
+        // 阻止页面表单的默认行为
+        e.preventDefault();
+        // 引用插件jquery.form.min.js对表单进行异步提交
+        $(this).ajaxSubmit({
+            url: "/api/v1.0/users/avatar",
+            type: "post",
+            dataType: "json",
+            headers: {
+                "X-CSRFToken": getCookie("csrf_token")
+            },
+            success: function (resp) {
+                if (resp.errno == "0"){
+                    //上传成功
+                    var aratarUrl= resp.data.aratar_url;
+                    $("#user-avatar").attr("src",aratarUrl);
+                }else{
+                    alert(resp.errmsg)
+                }
+            }
+        })
+
+    })
+
+});
