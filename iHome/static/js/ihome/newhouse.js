647447e47179f840d4a8d86f001dd3b9ecdb43ae
cdzm5211314 <123309778@qq.com>
2019-09-26 22:00:05 +0800

    城区信息获取的前端编写及前端js模版(atr-template)

--- iHome/static/js/ihome/newhouse.js
+++ iHome/static/js/ihome/newhouse.js
@@ -4,6 +4,20 @@ function getCookie(name) {
 }
 
 $(document).ready(function(){
-    // $('.popup_con').fadeIn('fast');
-    // $('.popup_con').fadeOut('fast');
-})
+    // 向后端获取城区信息
+    $.get("/api/v1.0/areas",function (resp) {
+        if (resp.errno == "0"){
+            // 获取到数据
+            var areas = resp.data;
+            // for (i=0; i<areas.length; i++) {
+            //     $("#area-id").append("<option value="+ areas[i].aid +">"+ areas[i].aname +"</option>");
+            // }
+            // 使用前端js模版
+            var html = template("areas-tmpl", {areas: areas})
+            $("#area-id").html(html);
+        }else{
+            alert(resp.errmsg)
+        }
+    }, "json")
+
+});
